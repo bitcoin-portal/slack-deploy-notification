4bd37e5f086866c1e932fa9a06f210d621cd5d0b
ncribt <ncribt@icloud.com>
2020-03-07 10:15:16 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
+
+try {
+  const slackWebhook = core.getInput("slack-webhook");
+  const jobStatus = core.getInput("job-status");
+  const deployedProjectUrl = core.getInput("deployed-project-url");
+
+  const githubUsername = github.context.actor;
+  const repositoryName = github.context.repo.repo;
+  const repositoryBranch = github.context.ref;
+
+  const successText = `Built and deployed successfully: ${deployedProjectUrl}`;
+  const failureText = `Build failed...`;
+  const cancelledText = `Build cancelled`;
+
+  let color = "good";
+  let text = `**${repositoryName}** | ${repositoryBranch} | triggered by **${githubUsername}**\n`;
+
+  if (jobStatus === "failure") {
+    color = "danger";
+    text += failureText;
+  } else if (jobStatus === "cancelled") {
+    color = "warning";
+    text += cancelledText;
+  } else {
+    text += successText;
+  }
+
+  const body = {
+    attachments: [
+      {
+        color,
+        text
+      }
+    ]
+  };
+
+  axios.post(slackWebhook, body);
+} catch (error) {
+  core.setFailed(error.message);
+}
